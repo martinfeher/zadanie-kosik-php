fd9e8f1f24dbe9bf545e9ccad9b46a5b23dec138
MARTIN\mnfeh <martin.feher@outlook.com>
2024-01-24 18:47:23 +0100

    update ShoppingCart, ShoppingCartItem

--- src/ShoppingCart.php
+++ src/ShoppingCart.php
@@ -4,5 +4,52 @@ namespace Eworkssk\ZadaniePhp;
 
 class ShoppingCart implements ShoppingCartInterface
 {
-    // todo: implement interface
+    private array $cartItems = array();
+
+    /**
+     * Add ShoppingCartItem instance to cart
+     */
+    public function add(ShoppingCartItem $shoppingCartItem) : void {
+
+        if (!isset($this->cartItems[$shoppingCartItem->id])) {
+            $this->cartItems[$shoppingCartItem->id] = $shoppingCartItem;
+        } else {
+            $this->cartItems[$shoppingCartItem->id]->quantity += $shoppingCartItem->quantity;
+        }
+    }
+    
+    /**
+     * Remove ShoppingCartItem from cart by ID
+     * Throws exception if item not found in cart
+     */
+    public function remove(int $shoppingCartItemID) : void {
+
+        if (isset($this->cartItems[$shoppingCartItemID])) {
+            unset($this->cartItems[$shoppingCartItemID]);
+        } else {
+            throw new \Exception('shoppingCartItemID input not found in the cart items');
+        }
+      
+    }
+
+    /**
+     * Return total price of all items in cart
+     */
+    public function total() : float {
+
+        $totalSum = 0;
+        foreach ($this->cartItems as $cartItem) {
+            $totalSum += $cartItem->quantity * $cartItem->price;
+        }
+        return $totalSum;
+    }
+    
+    /**
+     * Return array of all cart items
+     * @return ShoppingCartItem[]
+     */
+    public function getItems() : array {
+        return $this->cartItems;
+    }
+
 }
--- src/ShoppingCartItem.php
+++ src/ShoppingCartItem.php
@@ -5,6 +5,39 @@ namespace Eworkssk\ZadaniePhp;
 class ShoppingCartItem
 {
     public int $id;
+    public string $name;
+    public float $price;
+    public int $quantity;
+
+    public function __construct($id, $name, $price, $quantity) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+        $this->quantity = $quantity;
+    }
     
-    // todo: custom implementation
+    public function getName() {
+        return $this->name;
+    }
+    
+    public function setName($name) {
+        $this->name = $name;
+    }
+    
+    public function getPrice() {
+        return $this->price;
+    }
+    
+    public function setPrice($price) {
+        $this->price = $price;
+    }
+
+    public function getQuantity() {
+        return $this->quantity;
+    }
+    
+    public function setQuantity($quantity) {
+        $this->quantity = $quantity;
+    }
+    
 }
